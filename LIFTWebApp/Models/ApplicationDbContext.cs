8531a7b2ca4add3ede3910d7d9a68c09d64e47c8
shuayb badoolah <shuayb.b99@gmail.com>
2019-11-19 22:07:50 -0500

    added AppUser model

--- LIFTWebApp/Models/ApplicationDbContext.cs
+++ LIFTWebApp/Models/ApplicationDbContext.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LIFTWebApp.Models
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
