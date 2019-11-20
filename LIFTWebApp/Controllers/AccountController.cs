7ca04e028c386cdb49bbf5ac80a2b1b8a86ee2f8
shuayb badoolah <shuayb.b99@gmail.com>
2019-11-20 00:41:55 -0500

    Added view model and controller for registration page, as well routing,  still doesnt work completely

--- LIFTWebApp/Controllers/AccountController.cs
+++ LIFTWebApp/Controllers/AccountController.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LIFTWebApp.Models;
+using LIFTWebApp.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LIFTWebApp.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+              
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (ModelState.IsValid) 
+            {
+                var user = new AppUser { UserName = vm.Username, Email = vm.Email };
+                var result = await _userManager.CreateAsync(user, vm.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else 
+                {
+                    foreach (var error in result.Errors) 
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                }
+            }
+            return View(vm);
         }
     }
 }
