b3d27a563a3bf6a3939b8d640109ee0525c95044
shuayb badoolah <shuayb.b99@gmail.com>
2019-11-19 22:49:44 -0500

    added new Dbcontext

---
+++ LIFTWebApp/Models/UserDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LIFTWebApp.Models
+{
+    public class UserDataContext : DbContext
+    {
+        public UserDataContext(DbContextOptions<UserDataContext> options): base (options)
+        {
+
+        }
+    }
+}
