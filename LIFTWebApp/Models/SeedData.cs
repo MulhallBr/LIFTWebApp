1b8be2d29a3da17bdd8137597b22205f4a48aa93
Brian <brian.mulhall@hotmail.com>
2019-11-19 22:54:53 -0500

    Quick commit for testing.

--- LIFTWebApp/Models/SeedData.cs
+++ LIFTWebApp/Models/SeedData.cs
@@ -10,7 +10,9 @@ namespace LIFTWebApp.Models
     public static class SeedData
     {
 
-        public static void EnsurePopulated(IApplicationBuilder app)
+
+
+            public static void EnsurePopulated(IApplicationBuilder app)
         {
             ApplicationDbContext context = app.ApplicationServices
                 .GetRequiredService<ApplicationDbContext>();
@@ -21,37 +23,36 @@ namespace LIFTWebApp.Models
 
                     new Lift { 
                         UserID = 1,
-                        DateTimePerformed = "2019-11-15T13:45:30", 
+                        DateTimePerformed = "2019-11-15", 
                         ExercisePerfromed = "Bench Press", 
                         Weight = 150, Reps = 5 },
                     new Lift {
                         UserID = 1,
-                        DateTimePerformed = "2019-11-15T13:50:00", 
+                        DateTimePerformed = "2019-11-15", 
                         ExercisePerfromed = "Bench Press", 
                         Weight = 150, Reps = 7 },
                     new Lift {
                         UserID = 1,
-                        DateTimePerformed = "2019-11-15T13:52:00", 
+                        DateTimePerformed = "2019-11-15", 
                         ExercisePerfromed = "Bench Press", 
                         Weight = 150, Reps = 4 },
-
                     new Lift {
                         UserID = 1,
-                        DateTimePerformed = "2019-11-15T13:59:00",
+                        DateTimePerformed = "2019-11-15",
                         ExercisePerfromed = "Bicep Curls",
                         Weight = 50, Reps = 10 },
                     new Lift {
                         UserID = 1,
-                        DateTimePerformed = "2019-11-15T14:02:00",
+                        DateTimePerformed = "2019-11-15",
                         ExercisePerfromed = "Bicep Curls",
                         Weight = 50, Reps = 10 },
                     new Lift {
                         UserID = 1,
-                        DateTimePerformed = "2019-11-15T14:06:00",
+                        DateTimePerformed = "2019-11-15",
                         ExercisePerfromed = "Bicep Curls",
                         Weight = 50, Reps = 10 }
-
                     );
+
                 context.SaveChanges();
             }
         }
